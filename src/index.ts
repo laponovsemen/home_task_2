159ae7b896d087698e92f5cb048d8e7e3289fe20
Laponov Semen <johndoe@example.com>
2023-04-05 18:01:28 +0300

    edit index.ts file with initial start

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const port = process.env.PORT || 3000
+const app = express()
+
+app.use(express.json())
+const startApp =  () => {
+    app.listen(port, () => {
+        console.log(`app started on ${port} port`)
+    })
+}
