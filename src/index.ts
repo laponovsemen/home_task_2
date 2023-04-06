83ed7d069a6146416987fb72cffcad5b0e86dc6b
Laponov Semen <johndoe@example.com>
2023-04-06 13:16:10 +0300

    Correction of mistakes

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ const app = express()
 
 app.use(express.json())
 app.use("", startRouter)
+
 const startApp =  () => {
     app.listen(port, () => {
         console.log(`app started on ${port} port`)
