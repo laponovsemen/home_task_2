4810b3c0e0673171590044c40ed50c98bc559d52
Laponov Semen <johndoe@example.com>
2023-04-06 12:01:04 +0300

    Correction of mistakes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, {Request, Response} from "express";
 
 
-const startRouter = express.Router()
+export const startRouter = express.Router()
 
 startRouter.get("", (req: Request, res: Response) => {
     res.status(200).send("API started")
---
+++ src/appTypes.ts
@@ -0,0 +1,35 @@
+
+
+export  type APIErrorResultType = {
+    errorsMessages : FieldErrorType[]
+
+}
+export type FieldErrorType = {
+    message: string | null, // Message with error explanation for certain field
+    field : string | null // What field/property of input model has error
+}
+export type BlogInputModel = {
+    name : string, //maxLength: 15
+    description : string,// maxLength: 500
+    websiteUrl : string // maxLength: 100 pattern: ^https://([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$
+}
+export type BlogViewModel = {
+    id : string,
+    name:	string,
+    description: string,
+    websiteUrl: string
+}
+export type PostInputModel = {
+    title : 	string, //    maxLength: 30
+    shortDescription: string, //maxLength: 100
+    content: string, // maxLength: 1000
+    blogId: string
+}
+export type PostViewModel = {
+    id:	string,
+    title:	string,
+    shortDescription:	string,
+    content:	string,
+    blogId:	string,
+    blogName:	string
+}
